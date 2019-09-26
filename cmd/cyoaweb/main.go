9ba8c16457e57ec396491ba137dba8fe93eee83d
Veg Svarg <veg.svarg@gmail.com>
2019-09-25 19:42:05 -0700

    added file open functionality for gopher.json, cli usage can pass --file flag to specify another file name

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+)
+
 func main()  {
-	
+	fileName := flag.String("file", "gopher.json", "the JSON file with the CYOA story")
+	flag.Parse()
+	fmt.Printf("Using the story in %s\n", *fileName)
+
+	// open file with filehandle
+	fileHandle, err := os.Open(*fileName)
+	// make sure there is no errors
+	// usually panicking is not a great idea...
+	if err  != nil {
+		panic(err)
+	}
+
 }
